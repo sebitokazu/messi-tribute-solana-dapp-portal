73b52efa539ce603125264790c63f8eb05f76502
Sebastian Itokazu <sebitokazu@gmail.com>
2021-11-10 17:35:42 -0300

    Solana integrations

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import './App.css';
 const TWITTER_BASE = `https://twitter.com`;
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_PROFILE = 'seba_itokazu';
+const YOUTUBE_EMBED_BASE = 'https://www.youtube.com/embed/';
+
+const TEST_GIFS = [
+	'https://www.youtube.com/embed/pYIGdxr-2hc',
+	'https://www.youtube.com/embed/X4Z_prRI9B4'
+]
 
 const App = () => {
 
   // State
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [videoList, setVideoList] = useState([]);
 
   /*
    * This function holds the logic for deciding if a Phantom Wallet is
@@ -47,6 +55,21 @@ const App = () => {
     }
   };
 
+  const onInputChange = (event) => {
+    const { value } = event.target;
+    setInputValue(value);
+  };
+
+  const sendVideo = async () => {
+    if (inputValue.length > 0) {
+      console.log('Video link:', inputValue);
+      let ref = inputValue.substring(inputValue.indexOf('watch?v=')+8);
+      let embedLink = YOUTUBE_EMBED_BASE + ref;
+    } else {
+      console.log('Empty input. Try again.');
+    }
+  };
+
   /*
    * We want to render this UI when the user hasn't connected
    * their wallet to our app yet.
@@ -58,6 +81,24 @@ const App = () => {
     >
       Connect to Wallet
     </button>
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <input 
+      type="text" 
+      placeholder="Enter YT link! (eg. https://www.youtube.com/watch?v=X4Z_prRI9B4)"
+      value={inputValue}
+      onChange={onInputChange} />
+      <button className="cta-button submit-gif-button" onClick={sendVideo}>Submit</button>
+      <div className="gif-grid">
+        {videoList.map((gif,idx) => (
+          <div className="gif-item" key={gif}>
+            <iframe src={gif}  frameBorder="0" title={`gif_${idx}`}/>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 
   /*
@@ -72,6 +113,17 @@ const App = () => {
     return () => window.removeEventListener('load', onLoad);
   }, []);
 
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching Video list...');
+      
+      // Call Solana program here.
+  
+      // Set state
+      setVideoList(TEST_GIFS);
+    }
+  }, [walletAddress]);
+
 
   return (
     <div className="App">
@@ -82,6 +134,7 @@ const App = () => {
             View the best moves from the best in the history right in the Solana Messiverse ✨
           </p>
           {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
